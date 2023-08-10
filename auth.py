c92bf777e2936d42d2127cb7e6878864559c5e2f
tomsenior20 <tomsenior20@gmail.com>
2023-08-10 15:39:37 +0100

    unit tests for login and register added

--- auth.py
+++ auth.py
@@ -1,16 +1,18 @@
-from flask import Flask, flash,render_template
+from flask import Flask, flash, render_template
 from method import *
 
 # Login Function
-def check_login(user_id,logInPassword):
+
+
+def check_login(user_id, logInPassword):
     # Check for null values in form data
-    if not all((user_id.strip(),logInPassword.strip())):
+    if not all((user_id.strip(), logInPassword.strip())):
         login_message = "Please fill in all fields."
     elif len(user_id.strip()) < 4 or len(logInPassword.strip()) < 4:
         login_message = "Password or Username is less than required characters"
     else:
         # Goes to method.py to validate against DB
-        check_login = check_login_check(user_id,logInPassword)
+        check_login = check_login_check(user_id, logInPassword)
         if check_login == "Logged In Successfully":
             login_message = check_login
         elif check_login == "invalid password" or "User does not exsist" or "Please Enter Valid Inputs":
@@ -19,16 +21,17 @@ def check_login(user_id,logInPassword):
 
 
 # Registers the user function
-def check_register(full_name,password,admin_privilege):
+def check_register(full_name, password, admin_privilege):
     # Check for null values in form data
-    if not all((full_name.strip(), password.strip() , admin_privilege.strip())):
+    if not all((full_name.strip(), password.strip(), admin_privilege.strip())):
         msg = "Please Fill out all Inputs"
     # Handles all scenarios to when char are less than 4 or admin priv is not equal to the string, removes whitespace using .strip()
     elif len(full_name.strip()) < 4 or len(password.strip()) < 4 or (admin_privilege.strip() != "yes" and admin_privilege.strip() != "no"):
         msg = 'one of the mininum inputs requirements havent been met'
     else:
         # Goes to Method.PY to fetch the data and check if exsists if not store.
-        returned_register_message = check_and_register_user(full_name,password,admin_privilege)
+        returned_register_message = check_and_register_user(
+            full_name, password, admin_privilege)
         # Check the Message returned and assign a msg to it
         if returned_register_message == "Success, User has been registered":
             msg = returned_register_message
@@ -38,10 +41,12 @@ def check_register(full_name,password,admin_privilege):
     return msg
 
 # Deletion of Row Function
+
+
 def check_delete(location):
     # Check if the location passed is null or not
     if not location:
-       delete_msg = 'Please fill out Location Input'
+        delete_msg = 'Please fill out Location Input'
     else:
         # Go to method.py and perform the SQL Script to delete
         attempt_delete = delete_row(location)
@@ -50,41 +55,45 @@ def check_delete(location):
             delete_msg = "Row Deleted"
         else:
             delete_msg = "error"
-    
+
     return delete_msg
 
 # Inserting Row Function
+
+
 def insert_row(location, comment):
     # Check if the location passed is null or not
     if not (location and comment):
         status_msg = 'Invalid inputs'
     else:
-        status_check_insert = inserting_row(location,comment)
-        #Checks if the response is succefull or not 
+        status_check_insert = inserting_row(location, comment)
+        # Checks if the response is succefull or not
         if status_check_insert == "success":
             status_msg = "Row Successfully Inserted"
         else:
             status_msg = "Erorr Inserting Row"
-            
+
     return status_msg
 
 # Update Row
-def update_row_attempt(currentLocation, newLocation,currentComment,newComment):
+
+
+def update_row_attempt(currentLocation, newLocation, currentComment, newComment):
     update_msg = None
     attempt_comment_update = None
     attempt_location_update = None
-    
+
     if currentComment and newComment and not (currentLocation and newLocation):
         attempt_comment_update = update_comment(currentComment, newComment)
     elif currentLocation and newLocation and not (currentComment or newComment):
-        attempt_location_update = update_location(currentLocation,newLocation)
+        attempt_location_update = update_location(currentLocation, newLocation)
     elif currentComment and newComment and currentLocation and newLocation:
         attempt_comment_update = update_comment(currentComment, newComment)
-        attempt_location_update = update_location(currentLocation,newLocation)
-        
-    if attempt_comment_update == 'Successfully Updated Row' or attempt_location_update  == 'Successfully Updated Row':
+        attempt_location_update = update_location(currentLocation, newLocation)
+
+    if attempt_comment_update == 'Successfully Updated Row' or attempt_location_update == 'Successfully Updated Row':
         update_msg = 'Update Successful'
     else:
-            update_msg = attempt_location_update or attempt_comment_update
-          
+        update_msg = attempt_location_update or attempt_comment_update
+
     return update_msg
--- testing.py
+++ testing.py
@@ -1,29 +1,69 @@
 import unittest
 from unittest.mock import patch
-from auth import check_login
+from auth import check_login, check_register
 
 
 class TestInsertRow(unittest.TestCase):
+    # Unit Test for Log in Methods
     @patch('auth.check_login')
-    def test_successful_login(self, mock_loggin_row):
+    def test_successful_login(self, mock_successfull_login):
+        mock_successfull_login = "success"
         # Passing in valid credentials in DB
         result = check_login("admin", "admin")
-        print("successfunction - " + result)
+        print("@test_successful_login - " + result)
         self.assertEqual(result, "Logged In Successfully")
 
-    @patch('auth.insert_row')
-    def test_failed_login(self, mock_loggin_row):
+    @patch('auth.check_login')
+    def test_failed_login(self, mock_invalid_login):
         # Setting Scenario for lower than char length
         result = check_login("sss", "ss")
-        print("character failure -  " + result)
+        print("@test_failed_login -  " + result)
         self.assertEqual(
             result, "Password or Username is less than required characters")
 
-    def test_invalid_inputs(self):
+    @patch('auth.check_login')
+    def test_invalid_inputs(self, mock_invalid_login):
         # Testing For Empty Inputs
         result = check_login("", "")
-        print("Empty string test - " + result)
+        print("@test_invalid_inputs - " + result)
         self.assertEqual(result, "Please fill in all fields.")
+
+    # Testing the Registering
+
+    # This needs to have unique entry each time or the test will fail
+    # @patch('auth.check_register')
+    # def test_valid_reg(self, mock_text_reg):
+    #     # Note Before each test these needs to be unique
+    #     result = check_register("dddd", "dddd", "yes")
+    #     print("@test_valid_reg - " + result)
+    #     self.assertEqual(result, "Success, User has been registered")
+
+    # Check for Greater than 4 char
+    @patch('auth.check_register')
+    def test_invalid_reg(self, mock_text_reg):
+        # Note this checks for < 4 characters
+        result = check_register("dd", "ddd", "yes")
+        print("@test_invalid_reg - " + result)
+        self.assertEqual(
+            result, 'one of the mininum inputs requirements havent been met')
+
+    # Checks for null entry
+    @patch('auth.check_register')
+    def test_null_reg(self, mock_text_reg):
+        # Note checks for null entries
+        result = check_register("", "", "")
+        print("@test_null_reg - " + result)
+        self.assertEqual(
+            result, 'Please Fill out all Inputs')
+
+    # Checks for valid name and password but incorrect admin
+    @patch('auth.check_register')
+    def test_invalid_admin_reg(self, mock_text_reg):
+        # Note checks for invalid admin entry
+        result = check_register("testuser", "testuser", "hello")
+        print("@test_invalid_admin_reg - " + result)
+        self.assertEqual(
+            result, 'one of the mininum inputs requirements havent been met')
 
 
 if __name__ == '__main__':
