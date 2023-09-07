0781922b6b8c3013ed8a59cd74d0e05de6836399
tomsenior20 <tomsenior20@gmail.com>
2023-09-07 17:28:00 +0100

    added try and catch for estabilishing sql connection

--- methods/method.py
+++ methods/method.py
@@ -7,35 +7,33 @@ import re
 # Selects the table and will pass in the table param
 def table_exists(cursor, table_name):
     cursor.execute(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
+        "SELECT name FROM sqlite_master WHERE type='table' AND name=?", (table_name,)
+    )
     return cursor.fetchone() is not None
+
 
 # Creates the tables on page load.
 # It will then loop over the array of create_table, for each it will execute via for loop
 
 
 def create_tables():
-    # Connect to the database (creates a new database if it doesn't exist)
-    conn = sqlite3.connect("database.db")
-    # Create a cursor object to execute SQL commands
-    cursor = conn.cursor()
     # Create a Array of tables to create
     create_table = [
-        '''
+        """
         CREATE TABLE IF NOT EXISTS users (
             ID INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id TEXT NOT NULL UNIQUE,
             Password TEXT NOT NULL,
             Admin TEXT NOT NULL
         )
-    ''',
-        '''
+    """,
+        """
         CREATE TABLE IF NOT EXISTS data (
             LOCATION STRING NOT NULL PRIMARY KEY,
-            COMMENT STRING NOT NULL 
+            COMMENT STRING NOT NULL
         )
-    ''',
-        '''
+    """,
+        """
             CREATE TABLE IF NOT EXISTS assignmentGroup(
             ID INTEGER PRIMARY KEY AUTOINCREMENT,
             LOCATION TEXT NOT NULL,
@@ -43,238 +41,317 @@ def create_tables():
             COMPANY STRING NOT NULL,
             FOREIGN KEY (LOCATION) REFERENCES data (LOCATION)
         )
-    '''
+    """,
     ]
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
     try:
         # then loop over the list
         for table in create_table:
             # Use regex to search for the table name in for loop
-            match = re.search(r'CREATE TABLE IF NOT EXISTS (\w+)', table)
+            match = re.search(r"CREATE TABLE IF NOT EXISTS (\w+)", table)
             if match:
                 table_name = match.group(1)
                 # Check if the table already exists
                 if not table_exists(cursor, table_name):
                     cursor.execute(table)
                     conn.commit()
+                else:
+                    return "Table Exists"
     except sqlite3.Error as exception:
         return "Error" + str(exception)
     finally:
         conn.close()
+
 
 # Runs on register form submit, checks if user exists = true then it will not allow to register
 # If the user doesn't exist then this will register the user.
 
 
 def check_and_register_user(user_id, password):
-    # Connect to the database
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    msg = None
-    # Check if the user with the given user_id already exists
-    cursor.execute("SELECT user_id FROM users WHERE user_id = ?", (user_id,))
-    existing_user = cursor.fetchone()
-    # if condition = true return the string
-    if existing_user:
-        # User is present in users table
-        msg = "User already exists in the database."
-    else:
-        # Write the user to the 'users' table if previous condition = false
-        if len(user_id) >= 4 and len(password) >= 4:
-            try:
-                # Assigns the parameters to an class
-                new_user = User(user_id, password, "no")
-                cursor.execute("INSERT INTO users (user_id, Password, Admin) VALUES (?, ?, ?)", (
-                    new_user.user_id, new_user.password, new_user.admin_privilege))
-                conn.commit()
-                msg = "Success, User has been registered"
-            except:
-                msg = "error"
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    try:
+        # Check if the user with the given user_id already exists
+        cursor.execute("SELECT user_id FROM users WHERE user_id = ?", (user_id,))
+        existing_user = cursor.fetchone()
+        # if condition = true return the string
+        if existing_user:
+            # User is present in users table
+            return "User already exists in the database."
         else:
-            msg = "Error Invalid Input"
-    conn.close()
-    return msg
+            # Write the user to the 'users' table if previous condition = false
+            if len(user_id) >= 4 and len(password) >= 4:
+                try:
+                    # Assigns the parameters to an class
+                    new_user = User(user_id, password, "no")
+                    cursor.execute(
+                        "INSERT INTO users (user_id, Password, Admin) VALUES (?, ?, ?)",
+                        (new_user.user_id, new_user.password, new_user.admin_privilege),
+                    )
+                    conn.commit()
+                    return "Success, User has been registered"
+                except:
+                    return "error"
+            else:
+                return "Error Invalid Input"
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    finally:
+        conn.close()
+
 
 # Check Login Credentials, strips the username and password for whitespace
 # Checks if the user exists, if does login, else throw error.
 
 
 def check_login_check(username, password):
-    # Connect to the database
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
     # Gets the parameters for the username & password and removes whitespace.
-    if username and username.strip() and password and password.strip():
-        # Decided to use placeholders, instead of a string for added security.
-        cursor.execute(
-            "SELECT * from users WHERE user_id = ? AND password = ?", (username, password))
-        # Fetchs one record using fetchone()
-        user = cursor.fetchone()
-        # Checks if the user exists
-        if user:
-            # Assigns each column to my class
-            retuned_record = User(user[1], user[2], user[3])
-            # Checks if the entered password & username match the record, return the relevant success / error message
-            if retuned_record.password == password and retuned_record.user_id == username:
-                loginMsg = "Logged In Successfully"
+    # Decided to use placeholders, instead of a string for added security.
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    try:
+        if username and username.strip() and password and password.strip():
+            cursor.execute(
+                "SELECT * from users WHERE user_id = ? AND password = ?",
+                (username, password),
+            )
+            # Fetchs one record using fetchone()
+            user = cursor.fetchone()
+            # Checks if the user exists
+            if user:
+                # Assigns each column to my class
+                retuned_record = User(user[1], user[2], user[3])
+                # Checks if the entered password & username match the record, return the relevant success / error message
+                if (
+                    retuned_record.password == password
+                    and retuned_record.user_id == username
+                ):
+                    return "Logged In Successfully"
+                else:
+                    return "Invalid password"
             else:
-                loginMsg = "Invalid password"
+                return "User does not exist"
         else:
-            loginMsg = "User does not exist"
-    else:
-        loginMsg = "Please Enter Valid Inputs"
+            return "Please Enter Valid Inputs"
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    finally:
+        conn.close()
 
-    conn.close()
-    return loginMsg
 
 # Display Data Method will select all from the table, loop over rows and create new data record
 
 
 def display_data():
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    # Executes the SQL Query
-    cursor.execute(
-        "SELECT data.LOCATION,data.comment,assignmentGroup.JOBROLE,assignmentGroup.COMPANY FROM data JOIN assignmentGroup ON data.LOCATION = assignmentGroup.LOCATION")
-    # Fetches all the rows associated to the query
-    rows = cursor.fetchall()
-    # Creates Empty array for the data
-    record_list = []
-    # For each row in the fetched rows, create new DataRecord object and assign values and append to previous object
-    for record in rows:
-        data_record = DataRecord(record[0], record[1], record[2], record[3])
-        record_list.append(data_record)
-    # Close Connection
-    conn.close()
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    try:
+        # Executes the SQL Query
+        cursor.execute(
+            "SELECT data.LOCATION,data.comment,assignmentGroup.JOBROLE,assignmentGroup.COMPANY FROM data JOIN assignmentGroup ON data.LOCATION = assignmentGroup.LOCATION"
+        )
+        # Fetches all the rows associated to the query
+        rows = cursor.fetchall()
+        # Creates Empty array for the data
+        record_list = []
+        # For each row in the fetched rows, create new DataRecord object and assign values and append to previous object
+        for record in rows:
+            data_record = DataRecord(record[0], record[1], record[2], record[3])
+            record_list.append(data_record)
+        # Close Connection
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    finally:
+        conn.close()
     # Returns the object
     return record_list
+
 
 # Retrieve a User Record, match record and then create a new user class
 
 
 def get_user_record(username, password):
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
     # Executes the SQL Query
-    cursor.execute(
-        "SELECT * FROM users WHERE user_id = ? and Password = ?", (username, password,))
-    user = cursor.fetchall()
+    try:
+        cursor.execute(
+            "SELECT * FROM users WHERE user_id = ? and Password = ?",
+            (
+                username,
+                password,
+            ),
+        )
+        user = cursor.fetchall()
 
-    matched_record = []
-    # Loop over all the records
-    for matched_user in user:
-        matched_user_record = User(
-            matched_user[1], matched_user[2], matched_user[3])
-        # Once i've created my user class append to list
-        matched_record.append(matched_user_record)
-    conn.close()
+        matched_record = []
+        # Loop over all the records
+        for matched_user in user:
+            matched_user_record = User(
+                matched_user[1], matched_user[2], matched_user[3]
+            )
+            # Once i've created my user class append to list
+            matched_record.append(matched_user_record)
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    finally:
+        conn.close()
     return matched_record
+
 
 # Deleting From SQL Script
 
 
 def delete_row(location):
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    try:
+        cursor.execute("select * from data WHERE Location = ?", (location,))
+        row_present = cursor.fetchall()
 
-    cursor.execute("select * from data WHERE Location = ?", (location,))
-    row_present = cursor.fetchall()
+        if row_present:
+            # Executes the SQL Query via DELETE
+            delete_row = [
+                ("DELETE FROM data WHERE Location = ?", (location,)),
+                ("DELETE FROM assignmentGroup WHERE Location = ?", (location,)),
+            ]
+            # loop over list per delete statement
+            for delete_row, values in delete_row:
+                cursor.execute(delete_row, values)
 
-    if row_present:
-        # Executes the SQL Query via DELETE
-        delete_row = [
-            ("DELETE FROM data WHERE Location = ?", (location,)),
-            ("DELETE FROM assignmentGroup WHERE Location = ?", (location,))
-        ]
-        # loop over list per delete statement
-        for delete_row, values in delete_row:
-            cursor.execute(delete_row, values)
+            conn.commit()
+            return "success"
+        else:
+            return "error"
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    finally:
+        conn.close()
 
-        conn.commit()
-        delete_msg = "success"
-    else:
-        delete_msg = "error"
-
-    conn.close()
-    return delete_msg
 
 # Inserting Row function
 
 
 def inserting_row(location, comment, jobRole, company):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    try:
+        cursor.execute("SELECT * FROM data where Location =?", (location,))
+        location_exists = cursor.fetchall()
+        # Checks if the record exsists
+        if location_exists:
+            return "Location already is in data, no duplicates allowed"
+        else:
+            # Execute the SQL query using wildcard
+            insert_row = {
+                (
+                    "INSERT INTO data (Location, Comment) VALUES (?, ?)",
+                    (location, comment),
+                ),
+                (
+                    "INSERT INTO assignmentGroup (LOCATION, JOBROLE, COMPANY) VALUES (?, ?, ?)",
+                    (location, jobRole, company),
+                ),
+            }
+            # Loop over my list for insert statement
+            for insert, values in insert_row:
+                cursor.execute(insert, values)
 
-    cursor.execute("SELECT * FROM data where Location =?", (location,))
-    location_exists = cursor.fetchall()
-    # Checks if the record exsists
-    if location_exists:
-        msg = "Location already is in data, no duplicates allowed"
-    else:
-
-        # Execute the SQL query using wildcard
-        insert_row = {
-            ("INSERT INTO data (Location, Comment) VALUES (?, ?)", (location, comment)),
-            ("INSERT INTO assignmentGroup (LOCATION, JOBROLE, COMPANY) VALUES (?, ?, ?)",
-             (location, jobRole, company))
-        }
-        # Loop over my list for insert statement
-        for insert, values in insert_row:
-            cursor.execute(insert, values)
-
-        conn.commit()
-        msg = "success"
-
-    return msg
+            conn.commit()
+            return "success"
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    finally:
+        conn.close()
 
 
 # Updating Location
 def update_location(oldLocation, newLocation):
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    # See if the record is present in the DB with the old location name
-    cursor.execute("select * from data WHERE Location = ?", (newLocation,))
-    exisitng_location = cursor.fetchall()
-    # Checks if the record exsists
-    if exisitng_location:
-        location_msg = "Error - New location already exists in the database"
-    else:
-        cursor.execute("select * from data WHERE Location = ?", (oldLocation,))
-        location_record = cursor.fetchall()
-
-        if location_record:
-            # Execute the SQL to update Location
-            cursor.execute(
-                "UPDATE data SET Location =? WHERE Location = ? ", (newLocation, oldLocation))
-            conn.commit()
-            location_msg = "Successfully Updated Row"
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    try:
+        # See if the record is present in the DB with the old location name
+        cursor.execute("select * from data WHERE Location = ?", (newLocation,))
+        exisitng_location = cursor.fetchall()
+        # Checks if the record exsists
+        if exisitng_location:
+            return "Error - New location already exists in the database"
         else:
-            location_msg = "Error - Record with Location not found"
+            cursor.execute("select * from data WHERE Location = ?", (oldLocation,))
+            location_record = cursor.fetchall()
 
-    return location_msg
+            if location_record:
+                # Execute the SQL to update Location
+                cursor.execute(
+                    "UPDATE data SET Location =? WHERE Location = ? ",
+                    (newLocation, oldLocation),
+                )
+                conn.commit()
+                return "Successfully Updated Row"
+            else:
+                return "Error - Record with Location not found"
+    except:
+        print("Error")
+    finally:
+        conn.close()
+
 
 # Updating comment
 
 
 def update_comment(oldComment, newComment):
-    conn = sqlite3.connect("database.db")
-    cursor = conn.cursor()
-    # See if the record is present in the DB with the old comment
-    cursor.execute("select * from data WHERE Comment = ?", (newComment,))
-    existing_comment = cursor.fetchall()
-    # If the record is existing then we don't want to over ride it / throw error
-    if existing_comment:
-        comment_msg = "Error - New Comment already exists in the database"
-    else:
-        # Selects the all comment from the DB
-        cursor.execute("select * from data WHERE Comment = ?", (oldComment,))
-        comment_record = cursor.fetchall()
-
-        if comment_record:
-            cursor.execute(
-                "UPDATE data SET Comment = ? WHERE Comment = ? ", (newComment, oldComment))
-            conn.commit()
-            comment_msg = "Successfully Updated Row"
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    try:
+        # See if the record is present in the DB with the old comment
+        cursor.execute("select * from data WHERE Comment = ?", (newComment,))
+        existing_comment = cursor.fetchall()
+        # If the record is existing then we don't want to over ride it / throw error
+        if existing_comment:
+            return "Error - New Comment already exists in the database"
         else:
-            comment_msg = "Error - Record with Comment not found"
+            # Selects the all comment from the DB
+            cursor.execute("select * from data WHERE Comment = ?", (oldComment,))
+            comment_record = cursor.fetchall()
 
-    return comment_msg
+            if comment_record:
+                cursor.execute(
+                    "UPDATE data SET Comment = ? WHERE Comment = ? ",
+                    (newComment, oldComment),
+                )
+                conn.commit()
+                return "Successfully Updated Row"
+            else:
+                return "Error - Record with Comment not found"
+    except sqlite3.Error as exception:
+        print("Error" + str(exception))
+    finally:
+        conn.close()
